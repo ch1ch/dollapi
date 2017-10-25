10f8293d452f2128ce23f49fdf356b6c576f53b6
fanghexu <fanghexu@kokozu.net>
2017-10-25 15:48:51 +0800

    添加 updateMachineVideo

--- src/main/java/com/dollapi/controller/MachineController.java
+++ src/main/java/com/dollapi/controller/MachineController.java
@@ -1,6 +1,7 @@
 package com.dollapi.controller;
 
 import com.dollapi.domain.MachineInfo;
+import com.dollapi.mapper.MachineInfoMapper;
 import com.dollapi.service.MachineService;
 import com.dollapi.util.ApiContents;
 import com.dollapi.util.Results;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -17,10 +19,27 @@ public class MachineController {
     @Autowired
     private MachineService machineService;
 
+    @Autowired
+    private MachineInfoMapper machineInfoMapper;
+
     @RequestMapping("/getMachineList")
     public Results getMachineList() {
         List<MachineInfo> list = machineService.getMachineInfoList();
         return new Results(ApiContents.NORMAL.value(), ApiContents.NORMAL.desc(), list);
+    }
+
+
+    @RequestMapping("/updateMachineVideo")
+    public Results updateMachineVideo(HttpServletRequest request) {
+        Long machineId = request.getParameter("machineId") == null ? null : Long.valueOf(request.getParameter("machineId"));
+        String video1 = request.getParameter("video1") == null ? null : request.getParameter("video1").toString();
+        String video2 = request.getParameter("video2") == null ? null : request.getParameter("video2").toString();
+        MachineInfo info = new MachineInfo();
+        info.setId(machineId);
+        info.setVideo1(video1);
+        info.setVideo2(video2);
+        machineInfoMapper.update(info);
+        return new Results(ApiContents.NORMAL.value(), ApiContents.NORMAL.desc());
     }
 
 }
--- src/main/java/com/dollapi/mapper/MachineInfoMapper.java
+++ src/main/java/com/dollapi/mapper/MachineInfoMapper.java
@@ -2,10 +2,11 @@ package com.dollapi.mapper;
 
 import com.dollapi.domain.MachineInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 @Mapper
 public interface MachineInfoMapper {
 
